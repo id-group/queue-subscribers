778cc7851bf3534e0050e849e30ee1de5f4e07b1
id-group <id-group@yandex.ru>
2023-09-05 19:14:57 +0300

    init

---
+++ client/src/main/java/ru/idgroup/qu/client/ClientApplication.java
@@ -0,0 +1,12 @@
+package ru.idgroup.qu.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+}
